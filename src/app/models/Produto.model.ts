266cdbc49cc40e75a6bee6c10a68eaeacb5f2e3f
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2024-05-12 18:05:38 -0300

    Service de consumo de API - 2: Serviço API de produtos
    Service de consumo de API - 2: Serviço API de produtos

--- src/app/models/Produto.model.ts
+++ src/app/models/Produto.model.ts
@@ -6,6 +6,10 @@ export class produto{
   preco: number = 0;
 
   constructor(id:number, produto:string, descricao:string, foto:string, preco:number){
-    
+    this.id = id;
+    this.produto = produto;
+    this.descricao = descricao;
+    this.foto = foto;
+    this.preco = preco;
   }
 }
---
+++ src/app/produto.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+private url = "http://localhost:3000/produtos";
+
+  constructor(private _httpClient: HttpClient) { }
+
+  getProduto(id:any): Observable<Produto>{
+    const urlIdProduto = `${this.url}?id={id}`;
+    return ;
+  }
+}
