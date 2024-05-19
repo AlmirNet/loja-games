8bcd0e0006bf3467945b7c7e003bc4efa71bc26b
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2024-05-19 16:00:01 -0300

    Service de consumo de API - 3: Rotas e CRUD
    Service de consumo de API - 3: Rotas e CRUD

--- src/app/restrito/restrito-routing.module.ts
+++ src/app/restrito/restrito-routing.module.ts
@@ -4,11 +4,23 @@ import { CadastroProdutoComponent } from "./cadastro-produto/cadastro-produto.co
 import { ListaProdutoComponent } from "./lista-produto/lista-produto.component";
 import { AtualizaProdutoComponent } from "./atualiza-produto/atualiza-produto.component";
 import { RestritoComponent } from "./restrito.component";
+import { NgModule } from "@angular/core";
 const restritoRoutes: Routes = [
     {
         path: 'restrito', component: RestritoComponent, children: [
             { path: 'cadastro', component: CadastroProdutoComponent},
-            { path: 'lista', component: AtualizaProdutoComponent}
+            { path: 'lista', component: ListaProdutoComponent},
+            { path: 'editar/:id', component: AtualizaProdutoComponent}
         ]
-    }
+    },
+
+    { path: '', redirectTo: '/restrito/lista', pathMatch: 'full' }
 ]
+
+@NgModule({
+    imports: [RouterModule.forChild(restritoRoutes)],
+    exports: [RouterModule]
+})
+
+export class RestritoRoutingModule{
+}
